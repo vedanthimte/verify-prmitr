f44d6d6f3f19f291b03cf05e817ee423927542b5
Vedant Himte <112320570+vedanthimte@users.noreply.github.com>
2025-09-22 01:43:27 +0000

    Updates

--- verify-himtetraders-app/src/supabase.js
+++ verify-himtetraders-app/src/supabase.js
@@ -1,9 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-// const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://cdlkwjyzokfjtkawroyu.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkbGt3anl6b2tmanRrYXdyb3l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0MzU4OTAsImV4cCI6MjA3NDAxMTg5MH0.DB3OcVzBYdSsp8X4Lws2y41ikG9qeLPpbJ-F-4TBwq0'
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+
 
